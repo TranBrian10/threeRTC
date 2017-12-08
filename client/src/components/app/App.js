41aece93aeb0b48d2908b10a68e538da76b59904
TranBrian10 <tranbrian10@gmail.com>
2017-12-08 03:42:57 -0500

    detect user agent

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -3,6 +3,19 @@ import logo from '../../assets/logo.svg';
 import './App.css';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { userAgent: 'None' };
+	}
+
+	componentDidMount() {
+		fetch('/user-agent')
+			.then(res => res.json())
+			.then(responseJson => this.setState(() => {
+				return { userAgent: responseJson.userAgent };
+			}));
+	}
+
 	render() {
 		return (
 			<div className="App">
@@ -10,7 +23,7 @@ class App extends Component {
 					<img src={logo} className="App-logo" alt="logo" />
 					<h1 className="App-title">Welcome to threeRTC</h1>
 				</header>
-				<p className="App-intro">Desktop</p>
+				<p className="App-intro">{this.state.userAgent} detected</p>
 			</div>
 		);
 	}
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res) {
 	res.render('index', { title: 'threeRTC' });
 });
 
+/* GET user agent */
+router.get('/user-agent', function(req, res) {
+	const userAgent = req.header('user-agent');
+
+	if (/mobile/i.test(userAgent)) {
+		res.send({ userAgent: 'Mobile' });
+	}
+
+	res.send({ userAgent: 'Desktop' });
+});
+
 module.exports = router;
