4e1b7d29ba9e1af70395b6cef1f473e1ff40c547
Mohammad Nabizadeh <m2nabiza@uwaterloo.ca>
2018-01-26 15:22:42 -0500

    Implemented basic collision logic.

--- client/src/game-logic/main.js
+++ client/src/game-logic/main.js
@@ -1,8 +1,37 @@
 import * as THREE from 'three';
+import * as CANNON from 'cannon';
 
 let webrtc;
 let gameInstance = null;
 let camera, scene, renderer;
+let world;
+let timeStep = 1/60;
+
+function initCannon() {
+	world = new CANNON.World();
+	world.gravity.set(0, 0, 0);
+	world.broadphase = new CANNON.NaiveBroadphase();
+	world.solver.iterations = 10;
+
+	var sphere1 = initSphereCannon(world, 2, new CANNON.Vec3(0, 0, 100), 2000);
+	sphere1.angularVelocity.set(0, 1, 0);
+
+	var sphere2 = initSphereCannon(world, 2, new CANNON.Vec3(0, 0, 0), 1);
+	sphere2.angularVelocity.set(0, -1, 0);
+	sphere2.applyImpulse(new CANNON.Vec3(0, 0, 60), new CANNON.Vec3(0, 0, 0));
+}
+
+function initSphereCannon(world, radius, pos, mass) {
+	var sphereBody = new CANNON.Body({
+		mass, // kg
+		position: pos,
+		shape: new CANNON.Sphere(radius)
+	});
+
+	world.addBody(sphereBody);
+
+	return sphereBody;
+}
 
 function init() {
 	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 1000);
@@ -16,10 +45,20 @@ function init() {
 	const worldPivotPos = camera.localToWorld(cameraPivotPos);
 
 	const handPivot = new THREE.Object3D();
+	handPivot.name = 'handPivot';
 	handPivot.position.set(worldPivotPos.x, worldPivotPos.y, worldPivotPos.z);
 	scene.add(handPivot);
 
 	initBat(handPivot, 55);
+
+	const spherePivot = new THREE.Object3D();
+	spherePivot.name = 'spherePivot';
+	spherePivot.position.set(0, 0, 0);
+	scene.add(spherePivot);
+
+	initSphere(spherePivot, 2, 'sphere1');
+
+	initSphere(spherePivot, 2, 'sphere2');
 
 	const handRotationHandler = createHandRotationHandler(handPivot);
 	this.addOrientationDataHandler(handRotationHandler);
@@ -41,17 +80,43 @@ function initBat(pivot, length) {
 	const material = new THREE.MeshNormalMaterial({ wireframe: true });
 	const batMesh = new THREE.Mesh(geometry, material);
 
+	batMesh.name = 'batty';
 	batMesh.translateY(length / 2);
 	pivot.add(batMesh);
+}
+
+function initSphere(pivot, radius, name) {
+	const geometry = new THREE.SphereGeometry(radius, 32, 32);
+	const material = new THREE.MeshNormalMaterial({ wireframe: true });
+	const sphereMesh = new THREE.Mesh(geometry, material);
+
+	sphereMesh.name = name;
+	pivot.add(sphereMesh);
 }
 
 function animate() {
 	requestAnimationFrame(animate);
 
-	//mesh.rotation.x += 0.01;
-	//mesh.rotation.y += 0.02;
+	updatePhysics();
 
 	renderer.render(scene, camera);
+}
+
+function updatePhysics() {
+
+	// Step the physics world
+	world.step(timeStep);
+
+	// Copy coordinates from Cannon.js to Three.js
+	var bodies = world.bodies;
+
+	var sphereMesh1 = scene.getObjectByName('spherePivot').getObjectByName('sphere1');
+	sphereMesh1.position.copy(bodies[0].position);
+	sphereMesh1.quaternion.copy(bodies[0].quaternion);
+
+	var sphereMesh2 = scene.getObjectByName('spherePivot').getObjectByName('sphere2');
+	sphereMesh2.position.copy(bodies[1].position);
+	sphereMesh2.quaternion.copy(bodies[1].quaternion);
 }
 
 class Game {
@@ -61,6 +126,7 @@ class Game {
 		this.betaOffset = 0;
 
 		init.call(this);
+		initCannon.call(this);
 		animate.call(this);
 	}
 
